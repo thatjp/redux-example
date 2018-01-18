37b8a974a1f752dd6eb0cbba84afe3ffbffcb481
github username <jpharris.contact@gmail.com>
2018-01-18 12:34:21 -0500

    git test

--- src/containers/App.js
+++ src/containers/App.js
@@ -47,7 +47,8 @@ handleClick() {
         
         <TopNav click={this.handleClick.bind(this)}/>
         <Bio info={profile} />
-        <p id="test">This element is styled through SASS</p>
+        <p class="test">This element is styled through SASS</p>
+        <p class="test">Test Elem.</p>
       </div> 
     );
   }
